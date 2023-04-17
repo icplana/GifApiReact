a4f88bbc3fad269c96e5b0c622dd88667c217e80
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-04-17 10:04:05 +0200

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'       no es necesario desde la version 17 de React
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+    </>
+  )
+}
+
+
+
+// const apiKey = 'MfhRWwqIRA55zbjQTF4CV9EYZeYM47RE'
